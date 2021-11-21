10f9f46a78a8d9dfb4ccf75adb5980d282ae193b
Seong1-Jo <seong12yada@gmail.com>
2021-11-21 23:24:34 +0900

    9 vue appbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      title 수정
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/xxx">xxx</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon />
+      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-spacer/><!--맨끝으로 보내는 효과-->
+      <v-btn icon to="/about">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn icon to="/"> <!-- 홈으로 가는 -->
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-
-</style>
+<script>
+export default {
+  name: 'App'
+}
+</script>
