0a4c0d70b2dc1fc8f9fe7854be8ff12ca8046053
Seong1-Jo <seong12yada@gmail.com>
2021-12-15 22:23:36 +0900

    12commpent

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,45 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon />
-      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/> <!--토글효과 false true를 번갈아가면서 -->
+      <site-title :title="title"></site-title>
       <v-spacer/><!--맨끝으로 보내는 효과-->
-      <v-btn icon to="/about">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-btn icon to="/"> <!-- 홈으로 가는 -->
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+     <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <site-menu></site-menu>
+    </v-navigation-drawer>
     <v-content>
       <router-view/>
     </v-content>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
+
 export default {
-  name: 'App'
+  components: { SiteTitle, SiteFooter, SiteMenu },
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      title: '나의 타이틀입니다',
+      footer: 'footer입니다'
+    }
+  }
 }
 </script>
